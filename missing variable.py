7702116023372ab497c9c187e78af879b3899958
Shristi1104 <sahashristi2004@gmail.com>
2025-04-27 15:10:35 +0530

    Add files via upload

---
+++ missing variable.py
@@ -0,0 +1,11 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import numpy as np 
+... import pandas as pd 
+... from sklearn.impute import SimpleImputer
+... dataset = pd.read_csv('pima-indians-diabetes.csv')
+... missing_data = dataset.isnull().sum()
+... print("Missing data: \n", missing_data)
+... imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+... imputer.fit(dataset)
+... dataset_imputed = imputer.transform(dataset)
